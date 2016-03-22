d4e27994bfcda4464997ac90f16f7885d3d6d860
raineryue <it_jinlei.yue@163.com>
2016-03-22 11:16:21 +0800

    Initial Commit

---
+++ 20160322001-Quartz2D-DrawCakeShape/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160322001-Quartz2D-DrawCakeShape
+//
+//  Created by Rainer on 16/3/22.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
